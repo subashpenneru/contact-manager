00d977ff9f799d8aa3fccddf9f4fe8d3b02a3401
Subash Penneru <subashpenneru@gmail.com>
2020-12-25 08:57:31 +0530

    Links updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,9 +6,9 @@ const Header = ({ branding }) => {
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0'>
       <div className='container'>
-        <a href='/' className='navbar-brand'>
+        <Link to='/' className='navbar-brand'>
           {branding}
-        </a>
+        </Link>
         <div>
           <ul className='navbar-nav mr-auto'>
             <li className='nav-item'>
